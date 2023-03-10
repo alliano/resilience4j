139eb4370d4f590de2c52c96a13c6eee1747309e
alliano <allianoanonymous@gmail.com>
2023-03-10 15:38:15 +0900

    initial commit

---
+++ src/main/java/com/reselince4j/Reselince4jApplication.java
@@ -0,0 +1,13 @@
+package com.reselince4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reselince4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reselince4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reselince4j/Reselince4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reselince4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reselince4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
