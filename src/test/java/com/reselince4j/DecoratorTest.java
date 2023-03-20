46d9325ff61de5be33def000203c9392de042584
alliano <allianoanonymous@gmail.com>
2023-03-20 20:02:42 +0900

    add example for metrics

--- src/test/java/com/reselince4j/DecoratorTest.java
+++ src/test/java/com/reselince4j/DecoratorTest.java
@@ -1,9 +1,12 @@
 package com.reselince4j;
 
 import java.time.Duration;
+import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Test;
 
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadConfig;
 import io.github.resilience4j.decorators.Decorators;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
@@ -44,5 +47,42 @@ public class DecoratorTest {
         }
 
         Thread.sleep(10_000L);
+    }
+
+    @SneakyThrows
+    public String getName(){
+        log.info("Wait.....................");
+        throw new RuntimeException("sorry there is error");
+    }
+
+    @Test @SneakyThrows
+    public void testFallback() {
+
+        RateLimiter rateLimiter = RateLimiter.of("rateLimiter", RateLimiterConfig.custom()
+        .limitForPeriod(10)
+        .limitRefreshPeriod(Duration.ofMinutes(1))
+        .build());
+
+        Retry retry = Retry.of("retry", RetryConfig.custom()
+        .maxAttempts(10)
+        .waitDuration(Duration.ofMillis(10))
+        .build());
+
+        Bulkhead bulkhead = Bulkhead.of("bulkhead", BulkheadConfig.custom()
+        .maxConcurrentCalls(10)
+        .maxWaitDuration(Duration.ofSeconds(1))
+        .build());
+
+        Supplier<String> supplier = Decorators.ofSupplier(() -> getName())
+        .withBulkhead(bulkhead)
+        .withRetry(retry)
+        .withRateLimiter(rateLimiter)
+        /**
+         * jikalau semua modul yang kita tambahakan (rateLimiter, bulkhead, retry) error
+         * maka method waitFallback akan di eksekusi
+         */
+        .withFallback((throwable) -> "Sorry the name that you loking for not found")
+        .decorate();
+        System.out.println(supplier.get());
     }
 }
